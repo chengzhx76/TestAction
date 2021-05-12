7d27e72733241e19d2aad2251d995e574327e5a8
chengzhx76 <chengzhx@qq.com>
2021-05-12 18:39:57 +0800

    update

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const http = require('http');
 
+console.log(getIPAddress())
+
 const server = http.createServer((req, res) => {
+    console.log(req.connection.remoteAddress)
+
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(new Date());
+    res.write('' + new Date());
     res.end();
 }).listen(3000);
 
 setTimeout(() => {
   console.log("关闭 server")
-  server.close()
-}, 5000)
+  // server.close()
+}, 10000)
+
+
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for(var devName in interfaces){
+      var iface = interfaces[devName];
+      for(var i=0;i<iface.length;i++){
+          var alias = iface[i];
+          if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){
+              return alias.address;
+          }
+      }
+  }
+}
