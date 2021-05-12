ac00092789b719ddea3f458ea9d42c292e7182c7
chengzhx76 <chengzhx@qq.com>
2021-05-12 18:30:55 +0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(new Date());
     res.end();
 }).listen(3000);
+
+setTimeout(() => {
+  console.log("关闭 server")
+  server.close()
+}, 5000)
